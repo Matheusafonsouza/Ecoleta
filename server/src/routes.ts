369884ffc552dac6065be5d3123c0624c3394827
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-02 17:07:50 -0300

    added list all items route and improved yarn dev script

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,23 @@
 //imports
 import express from 'express';
+import knex from './database/connection';
 
 //init
 const routes = express.Router();
 
 //routes
-routes.get('/', (req, res) => {
-    return res.json({ message: "hello" });
+    //return all items from items table
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return res.json(serializedItems);
 });
 
 export default routes;
