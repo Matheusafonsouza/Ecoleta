123b9e963ebef5e7e5ec92dd3473e1bf1e8df759
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-02 17:31:57 -0300

    added create point route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,12 +12,53 @@ routes.get('/items', async (req, res) => {
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         };
     });
 
     return res.json(serializedItems);
+});
+
+    //create a point
+routes.post('/points', async (req, res) => {
+    const { 
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = req.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id,
+        }
+    });
+
+    await trx('point_items').insert(pointItems);
+
+    return res.json({ success: true });
 });
 
 export default routes;
