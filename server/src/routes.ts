d406cd7546f1bb7f78be2a4f25242773811696a4
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-02 19:52:04 -0300

    added items controller and refactorated routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,26 +2,16 @@
 import express from 'express';
 import knex from './database/connection';
 import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 //init
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 //routes
     //return all items from items table
-routes.get('/items', async (req, res) => {
-    const items = await knex('items').select('*');
-
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
-
-    return res.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
     //create a point
 routes.post('/points', pointsController.create);
