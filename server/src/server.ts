74bbe834a93aa189d87e57e547cc1c514a4cc167
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-02 16:58:36 -0300

    added images configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 //imports
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 //init
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 //listen
 app.listen(3333);
