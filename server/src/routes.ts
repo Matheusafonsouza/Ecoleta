c9591a86ebd3c506458833fde02c7da04560907a
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-02 19:49:46 -0300

    added points controller and refactorated routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,11 @@
 //imports
 import express from 'express';
 import knex from './database/connection';
+import PointsController from './controllers/PointsController';
 
 //init
 const routes = express.Router();
+const pointsController = new PointsController();
 
 //routes
     //return all items from items table
@@ -22,43 +24,6 @@ routes.get('/items', async (req, res) => {
 });
 
     //create a point
-routes.post('/points', async (req, res) => {
-    const { 
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = req.body;
-
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-    });
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id,
-        }
-    });
-
-    await trx('point_items').insert(pointItems);
-
-    return res.json({ success: true });
-});
+routes.post('/points', pointsController.create);
 
 export default routes;
