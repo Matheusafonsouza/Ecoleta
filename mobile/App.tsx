9375cc6608fc034fcb0eca231ba084f1a886863d
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-06 19:00:00 -0300

    added navigation to mobile app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 
-import Home from './src/pages/home';
+import Routes from './src/routes'
 
 export default function App() {
 
@@ -21,7 +21,7 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-      <Home />
+      <Routes />
     </>
   );
 }
