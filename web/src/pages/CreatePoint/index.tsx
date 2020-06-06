a18857f6da347a9c25fb0e0bbcdf0b6faeeb9019
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-05 22:49:29 -0300

    map integration at form

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -46,6 +47,14 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map  center={[-15.9840809,-47.9911217]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-15.9840809,-47.9911217]}/>
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
