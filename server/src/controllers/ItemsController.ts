ce419eed18d1b25f8a0479c1a69c504d0cad027c
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-06 19:30:51 -0300

    created mobile map page

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -12,7 +12,7 @@ export default class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`,
+                image_url: `http://localhost:192.168.0.2:3333/uploads/${item.image}`,
             };
         });
 
