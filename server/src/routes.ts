b0704f7dcc68259be24f73b019a6536b262d5324
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-16 21:46:12 -0300

    validating data

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 //imports
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 import multer from 'multer';
@@ -16,7 +17,25 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
     //create a point
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+    '/points', 
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+);
 
     //return all points
 routes.get('/points', pointsController.index);
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import routes from './routes';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 //init
 const app = express();
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 //listen
 app.listen(3333);
